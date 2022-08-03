93659e6f3daef0c41c2f1d9d07b71d7a050d7012
Gabriel <gabrielgonzalezdomeniconi@gmail.com>
2022-08-03 19:59:46 -0300

    fix: added database configuration, dotenv lib and testing route on postman

--- src/app/controllers/UserCreateController.js
+++ src/app/controllers/UserCreateController.js
@@ -1,3 +1,11 @@
-module.exports = () => {
-  console.log('user create');
+module.exports = async (request, response) => {
+  try {
+    const { email, password } = request.body;
+
+    console.log('user create', email, password);
+
+    return response.status(200).json({ email, password });
+  } catch (err) {
+    console.log(err);
+  }
 };
--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const app = require('./app/app');
+require('dotenv').config();
 
 const { MONGO_DB_URL } = process.env;
 
-mongoose.connect('mongodb://localhost:27017', {
+mongoose.connect(MONGO_DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(async () => {
