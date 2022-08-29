21934919dea6d068c57618634b74f6664fdf1ba0
Gabriel <gabrielgonzalezdomeniconi@gmail.com>
2022-08-28 22:24:54 -0300

    fixed error message

--- src/app/services/clients/ClientCreateService.js
+++ src/app/services/clients/ClientCreateService.js
@@ -1,8 +1,8 @@
 const ClientModel = require('../../models/ClientModel');
 
-module.exports = async (full_name, email, document) => {
+module.exports = async ({ full_name, email, document }) => {
   const clientFind = await ClientModel.findOne({ $or: [{ full_name }, { email }, { document }] });
-  if (clientFind) return 'Identificamos que seus dados já constam no sistema, gostaria de efetuar o cadastro? Caso não queira, prosseguir com a compra!';
+  if (clientFind) return { error_message: 'Identificamos que seus dados já constam no sistema, gostaria de efetuar o cadastro? Caso não queira, prosseguir com a compra!' };
 
   const clientCreate = await ClientModel.create({
     full_name,
--- src/app/services/users/UserCreateService.js
+++ src/app/services/users/UserCreateService.js
@@ -1,12 +1,12 @@
 const UserModel = require('../../models/UserModel');
 
-module.exports = async (
+module.exports = async ({
   username,
   full_name,
   document,
   email,
   password,
-) => {
+}) => {
   const userFind = await UserModel.findOne({
     $or: [
       { username },
@@ -16,7 +16,7 @@ module.exports = async (
       { password },
     ],
   });
-  if (userFind) { return 'Os dados inseridos já constam no sistema, por favor entrar em contato com o suporte!'; }
+  if (userFind) { return { error_message: 'Os dados inseridos já constam no sistema, por favor entrar em contato com o suporte!' }; }
 
   const createUser = await UserModel.create({
     username,
